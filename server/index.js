7a0edd049dc82df0f8de96b84ec78f2ebd2d3573
Haruqt <91443323+Haruqt@users.noreply.github.com>
2026-06-30 10:25:46 +0800

    allow all cors origins

--- server/index.js
+++ server/index.js
@@ -6,12 +6,7 @@ const { router: authRouter } = require('./routes/auth')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://da-cvrc-inventory-copy-2.vercel.app'
-  ]
-}))
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', authRouter)
